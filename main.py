776222a48d1abb691e2a28de5482108f10d28cf1
Jose Quiroz <JoseRQuiroz@gmail.com>
2018-11-15 03:32:44 -0600

    2nd version

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
@@ -12,13 +10,11 @@ db = SQLAlchemy(app)
 
 app.secret_key = '8jsu7YchIOpwKmd8'
 
-
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     content = db.Column(db.String(200))
-
 
 
     def __init__(self, title, content):
@@ -44,7 +40,6 @@ def blog():
             return render_template('post.html', blog = blog)
 
 
-
        # return render_template('post.html', title=title, content=content, title_error=title_error, body_error=body_error)
 
 #@app.route("/blog", methods=['GET'])
@@ -56,40 +51,35 @@ def blog():
 
 @app.route('/newpost', methods = ['GET'])
 def newpost():
+
     return render_template('newpost.html')
+
 
 @app.route('/newpost', methods=['POST'])
 def add_post():
+
     title = request.form['title']
     content = request.form['content']
 
-    title_error = ""
-    content_error = ""
+    if not title and not content:
+        return render_template('newpost.html', title_error="🚨You Must Include A Title", content_error="🚨You Must Include Blog Content")
 
-    if title == "":
-        title_error = "*You Must Include A Title"
-    if content == "":
-        content_error = "*You Must Include Blog Content"
-    if title_error != '' or content_error != '':
-        return render_template('newpost.html', title_error=title_error, content_error=content_error)
+    elif not title:
+        return render_template('newpost.html', title_error="🚨You Must Include A Title", content=content)
 
+    elif not content:
+        return render_template('newpost.html', title=title, content_error="🚨You Must Include Blog Content")
+
+        return render_template('/blog?id={0}'.format(Blog))
+#case 2 issue
     else:
         new_post = Blog(title, content)
         db.session.add(new_post)
         db.session.commit()
+        blog = new_post
 
-    #return redirect('/blog')
-    return render_template("post.html")
-    #return render_template('post.html', id = id, title = title, content = content)
+    return render_template('new_entry.html', blog = blog)
 
-
-#@app.route('/newpost', methods = ['GET'])
-#def newpost():
- #   return render_template('newpost.html')
-
-#@app.route('/blog', methods = ['GET'])
-#def homepage():
- #   return render_template('/blog.html')
 
 if __name__ == '__main__':
     app.run()
